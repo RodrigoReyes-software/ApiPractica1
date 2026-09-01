cb9db67dc78b42a5a709580d6286373f9ff08972
hanniarmz <hanny_rmzz@outlook.com>
2026-09-01 17:23:56 -0600

    Cree la conexion de  MySQL a Node

---
+++ src/database.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const mysql = require("mysql2/promise");
+
+const conexion = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+module.exports = conexion;
